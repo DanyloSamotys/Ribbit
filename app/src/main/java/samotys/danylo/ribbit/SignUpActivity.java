4707427573ee6a529f9e991d463fbe6a2c43206c
DanyloSamotys <Danylo@MBP-Danylo.home>
2015-03-16 14:25:38 +0100

    Added field validation to signUp screen(isEmpty())

--- app/src/main/java/samotys/danylo/ribbit/SignUpActivity.java
+++ app/src/main/java/samotys/danylo/ribbit/SignUpActivity.java
@@ -1,17 +1,57 @@
 package samotys.danylo.ribbit;
 
+import android.app.AlertDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
 public class SignUpActivity extends ActionBarActivity {
+
+    @InjectView(R.id.usernameField) EditText mUsername;
+    @InjectView(R.id.passwordField) EditText mPassword;
+    @InjectView(R.id.emailField) EditText mEmail;
+    @InjectView(R.id.signUpButton) Button mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.inject(this);
+
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = mUsername.getText().toString();
+                String password = mPassword.getText().toString();
+                String email = mEmail.getText().toString();
+
+                username = username.trim();
+                password = password.trim();
+                email = email.trim();
+
+                if (username.isEmpty() || email.isEmpty() || password.isEmpty()){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this)
+                        .setMessage(R.string.alert_dialog_message)
+                        .setTitle(R.string.error_dialod_title)
+                        .setPositiveButton(android.R.string.ok, null);
+
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                else {
+                    //create a new user
+                }
+            }
+        });
+
     }
 
 
